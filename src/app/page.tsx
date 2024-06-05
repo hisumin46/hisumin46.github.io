50eaf8ac4757d6f8bb5bd5f19c6fa5561df8320d
hisumin46 <rtn2001@naver.com>
2024-06-05 14:24:44 +0900

    feature: nextjs 프로젝트 생성
    - nextjs 프로젝트 생성
    - .gitignore 파일 추가

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <>
+    <div>Next.js 배포 테스트 및 gh-pages 브랜치 테스트</div>
+    <div>
+      {/*<Image*/}
+      {/*  className="mb-3.5"*/}
+      {/*  src="/next.svg"*/}
+      {/*  alt="Next.js Logo"*/}
+      {/*  width={180}*/}
+      {/*  height={37}*/}
+      {/*  priority*/}
+      {/*/>*/}
+    </div>
+  </>
+  );
+}
